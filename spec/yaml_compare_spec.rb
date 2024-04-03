3196b6c40e80341240d19325ff8670f34881245c
Dorian Marié <dorian@dorianmarie.com>
2024-04-04 01:54:19 +0200

    linting

--- spec/yaml_compare_spec.rb
+++ spec/yaml_compare_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 require "tempfile"
 
@@ -5,26 +7,34 @@ RSpec.describe "yaml-compare" do
   it "works" do
     tempfile_en = Tempfile.create
     File.write(tempfile_en, <<~YAML)
-    en:
-      users: Users
-      projects: Projects
+      en:
+        users: Users
+        projects: Projects
     YAML
 
     tempfile_fr = Tempfile.create
     File.write(tempfile_fr, <<~YAML)
-    fr:
-      users: Utilisateurs
-      tasks: Tâches
+      fr:
+        users: Utilisateurs
+        tasks: Tâches
     YAML
 
-    expect {
+    expect do
       system("bin/yaml-compare #{tempfile_en.path} #{tempfile_fr.path} en fr")
-    }.to output(<<~OUTPUT).to_stderr_from_any_process
+    end.to output(<<~OUTPUT).to_stderr_from_any_process
       missing key "projects" from #{tempfile_fr.path}
       missing key "tasks" from #{tempfile_en.path}
     OUTPUT
   ensure
-    File.delete(tempfile_en) rescue nil
-    File.delete(tempfile_fr) rescue nil
+    begin
+      File.delete(tempfile_en)
+    rescue StandardError
+      nil
+    end
+    begin
+      File.delete(tempfile_fr)
+    rescue StandardError
+      nil
+    end
   end
 end
